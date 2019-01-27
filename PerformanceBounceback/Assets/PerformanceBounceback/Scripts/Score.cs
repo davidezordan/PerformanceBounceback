12ce548d1786a65bff22f82473cbe54239be214c
Davide Zordan <info@davidezordan.net>
2019-01-27 09:39:37 +0000

    Adding starter project

---
+++ PerformanceBounceback/Assets/PerformanceBounceback/Scripts/Score.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Score : MonoBehaviour {
+	public GameManager gameManager;
+	private Text textComponent;
+	void Start () {
+	   gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
+	   textComponent = GetComponentInChildren<Text>();
+	}
+	
+	void Update () {
+           textComponent.text = "Score: " + gameManager.score.ToString();		
+	}
+}
